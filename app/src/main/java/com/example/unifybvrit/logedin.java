161d32662cc4c5b91ea38c247a1f0c3ffa9f93e8
bhanu <bhanukiran5102@gmail.com>
2022-04-29 23:03:47 +0530

    unify-bvrit

--- app/src/main/java/com/example/unifybvrit/logedin.java
+++ app/src/main/java/com/example/unifybvrit/logedin.java
@@ -2,13 +2,28 @@ package com.example.unifybvrit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class logedin extends AppCompatActivity {
-
+    private ImageButton button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logedin);
+        button2= (ImageButton) findViewById(R.id.imageButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view_home();
+            }
+        });
+    }
+    public void view_home() {
+        Intent intent=new Intent(this,hamburger.class);
+        startActivity(intent);
     }
 }
